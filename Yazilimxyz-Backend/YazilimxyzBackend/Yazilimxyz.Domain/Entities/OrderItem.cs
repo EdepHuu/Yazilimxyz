00db3d0c1c5ac29f1a791c873edfe447ca6c4ae7
Metehan Dündar <91695805+EdepHuu@users.noreply.github.com>
2025-07-27 15:40:25 +0300

    Feature/metehan/veritabani baglantilari (#12)
    * yanlış katmanlar silindi.
    
    * veritabanı bağlantıları yapıldı.
    
    * Entity'ler tekrar eklendi

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.Domain/Entities/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yazilimxyz.Domain.Entities
+{
+    public class OrderItem : BaseEntity
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int ProductVariantId { get; set; }
+        public ProductVariant ProductVariant { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+}
