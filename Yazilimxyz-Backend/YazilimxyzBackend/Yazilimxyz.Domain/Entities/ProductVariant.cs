055df70ed1c37341bb47ba8c6c9b48f1a6648d27
Mehmet Zahit Uyanik <146885210+uyanikzahit@users.noreply.github.com>
2025-07-27 13:36:24 +0300

    Entitylerin oluşturulması. (#9)

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.Domain/Entities/ProductVariant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yazilimxyz.Domain.Entities
+{
+    public class ProductVariant : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public int Stock { get; set; }
+    }
+}
