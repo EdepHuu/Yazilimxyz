53537ef0aad326f71406d20e8c056d6f0d1ec19a
cogulbusra <b.cogul@urunver.com.tr>
2025-07-29 22:31:17 +0300

    feat: add home page

--- Yazilimxyz-Frontend/frontend-web/src/app/page.tsx
+++ Yazilimxyz-Frontend/frontend-web/src/app/page.tsx
@@ -1,7 +1,67 @@
+import Button from "@/components/Button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container heading-xl text-main-gray bg-gray">home page</div>
+    <>
+      <section>
+        <div className="image-wrapper">
+          <img
+            src="/home-image-1.jpg"
+            alt="magaza resmi"
+            className="home-image"
+          />
+          <div className="image-overlay"></div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center space-y-2">
+            <h2 className="heading-xl">Seçili Ürünlerde</h2>
+            <p className="heading-3xl">%40 İndirim</p>
+            <Button bgColor="white" className="mt-32">
+              Alışverişe Başla
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="">
+          <h3 className="heading-lg-1 text-center">Yeni | Yaz Koleksiyonu</h3>
+
+          <div className="relative h-[498px] flex">
+            <img
+              src="/home-image-2.jpg"
+              alt=""
+              className="w-1/2 object-cover"
+            />
+            <img
+              src="/home-image-3.jpg"
+              alt=""
+              className="w-1/2 object-cover"
+            />
+            <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+              <Button bgColor="black">Şimdi Keşfet</Button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h3 className="heading-lg-1 text-center">Geri Dönüştür & Kazan</h3>
+        <div className="relative">
+          <img
+            src="/home-image-4.jpg"
+            alt=""
+            className="h-[554px] w-full object-cover"
+          />
+          <div className="absolute inset-0 bg-black/20 z-10"></div>
+          <div className="absolute inset-0 mt-64 flex flex-col items-center justify-center text-center px-4 z-20">
+            <h2 className="heading-md-1 text-white mb-4 max-w-2xl">
+              Kullanmadığın kıyafetleri gönder, ShopEase puanı olarak indirim
+              kazan
+            </h2>
+            <Button bgColor="black">Detaylı Bilgi İçin</Button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
--- Yazilimxyz-Frontend/frontend-web/src/components/Navbar.tsx
+++ Yazilimxyz-Frontend/frontend-web/src/components/Navbar.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+"use client";
 
-function Navbar() {
+import { FavoriteIcon, SearchIcon, ShopIcon, UserIcon } from "@/icons/icon";
+import Link from "next/link";
+
+const navLinks = ["Kadın", "Erkek", "Çocuk", "Outlet"];
+
+export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className="container hidden md:flex h-16 items-center justify-between">
+        <div className="flex space-x-2 md:space-x-4 lg:space-x-8">
+          {navLinks.map((link, index) => (
+            <Link
+              key={index}
+              href={`/${link.toLowerCase().replace(/\s+/g, "-")}`}
+            >
+              <span className="heading-sm-3 md:heading-md-4 cursor-pointer">
+                {link}
+              </span>
+            </Link>
+          ))}
+        </div>
 
-export default Navbar
+        <div className="text-xl font-bold tracking-wide text-gray-800">
+          ShopEase
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 right-0 pr-2 flex items-center ">
+              <SearchIcon className="w-3 h-3 text-gray-400" />
+            </div>
+            <input
+              type="text"
+              placeholder="arama yap"
+              className="w-full pl-2 pr-8 py-2 border-none bg-gray rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-gray-300"
+            />
+          </div>
+          <Link href="/login">
+            <UserIcon />
+          </Link>
+          <Link href="/sepet">
+            <ShopIcon />
+          </Link>
+        </div>
+      </nav>
+
+      <nav className="container md:hidden mt-8 flex flex-col">
+        <div className="flex-1">
+          <div className="flex items-center justify-center space-x-6">
+            <div className="text-4xl font-bold tracking-wide text-gray-800">
+              ShopEase
+            </div>
+            <Link href="/login">
+              <UserIcon />
+            </Link>
+            <Link href="/sepet">
+              <ShopIcon />
+            </Link>
+          </div>
+
+          <div className="relative w-full my-4">
+            <div className="absolute inset-y-0 right-0 pr-2 flex items-center ">
+              <SearchIcon className="w-3 h-3 text-gray-400" />
+            </div>
+            <input
+              type="text"
+              placeholder="arama yap"
+              className="w-full pl-2 pr-8 py-2 border-none bg-gray rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-gray-300"
+            />
+          </div>
+        </div>
+
+        <div className="w-full bg-white py-2 flex justify-center gap-8 fixed bottom-0 left-0">
+          <div className="flex flex-col items-center">
+            <div className="text-lg font-bold tracking-wide text-gray-800">
+              ShopEase
+            </div>
+            <Link href="/">Anasayfa</Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <SearchIcon />
+            <Link href="/kategoriler">Kategoriler</Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <ShopIcon />
+            <Link href="/sepet">Sepetim</Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <FavoriteIcon />
+            <Link href="/favoriler">Favoriler</Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
