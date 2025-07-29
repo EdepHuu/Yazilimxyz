5d4b6e5624b9239d791eea0bdd03d48fa0d7f83e
Harun <149734625+HarunIder10@users.noreply.github.com>
2025-07-29 11:32:15 +0300

    confilict (#23)
    * feat: add tailwind config
    
    * Katmanlar düzenlendi.
    
    * Update Yazilimxyz.BusinessLayer.csproj
    
    * Genel güncelleme. Katmanlar düzeltildi.
    
    * Katman güncellemesi.
    
    ---------
    
    Co-authored-by: cogulbusra <b.cogul@urunver.com.tr>
    Co-authored-by: Metehan Dündar <91695805+EdepHuu@users.noreply.github.com>
    Co-authored-by: Mehmet Zahit Uyanik <146885210+uyanikzahit@users.noreply.github.com>
    Co-authored-by: Büşra Çoğul <77806575+busracogul@users.noreply.github.com>

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.EntityLayer/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yazilimxyz.EntityLayer.Entities
+{
+    public class Order : BaseEntity
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public decimal ShippingFee { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string AddressTitle { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
