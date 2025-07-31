ddbaf13ba37bff9a7513fa582ea4df94997e3eb8
Mehmet Zahit Uyanik <146885210+uyanikzahit@users.noreply.github.com>
2025-07-31 15:07:53 +0300

    Manager added to the concrete folder in the business layer. (#37)

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/MerchantManager.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Yazilimxyz.BusinessLayer.Abstract;
+using Yazilimxyz.BusinessLayer.DTOs.Merchant;
+using Yazilimxyz.EntityLayer.Entities;
+
+namespace Yazilimxyz.BusinessLayer.Concrete
+{
+    public class MerchantManager : IMerchantService
+    {
+        private readonly IMerchantRepository _merchantRepository;
+        private readonly IMapper _mapper;
+
+        public MerchantManager(IMerchantRepository merchantRepository, IMapper mapper)
+        {
+            _merchantRepository = merchantRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResultMerchantDto>> GetAllAsync()
+        {
+            var merchants = await _merchantRepository.GetAllAsync();
+            return _mapper.Map<List<ResultMerchantDto>>(merchants);
+        }
+
+        public async Task<ResultMerchantDto> GetByIdAsync(int id)
+        {
+            var merchant = await _merchantRepository.GetByIdAsync(id);
+            return _mapper.Map<ResultMerchantDto>(merchant);
+        }
+
+        public async Task CreateAsync(CreateMerchantDto dto)
+        {
+            var merchant = _mapper.Map<Merchant>(dto);
+            await _merchantRepository.AddAsync(merchant);
+        }
+
+        public async Task UpdateAsync(UpdateMerchantDto dto)
+        {
+            var merchant = _mapper.Map<Merchant>(dto);
+            await _merchantRepository.UpdateAsync(merchant);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var merchant = await _merchantRepository.GetByIdAsync(id);
+            if (merchant != null)
+            {
+                await _merchantRepository.DeleteAsync(merchant);
+            }
+        }
+    }
+}
---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/OrderItemManager.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Yazilimxyz.BusinessLayer.Abstract;
+using Yazilimxyz.BusinessLayer.DTOs.OrderItem;
+using Yazilimxyz.EntityLayer.Entities;
+
+namespace Yazilimxyz.BusinessLayer.Concrete
+{
+    public class OrderItemManager : IOrderItemService
+    {
+        private readonly IOrderItemRepository _orderItemRepository;
+        private readonly IMapper _mapper;
+
+        public OrderItemManager(IOrderItemRepository orderItemRepository, IMapper mapper)
+        {
+            _orderItemRepository = orderItemRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResultOrderItemDto>> GetAllAsync()
+        {
+            var items = await _orderItemRepository.GetAllAsync();
+            return _mapper.Map<List<ResultOrderItemDto>>(items);
+        }
+
+        public async Task<ResultOrderItemDto> GetByIdAsync(int id)
+        {
+            var item = await _orderItemRepository.GetByIdAsync(id);
+            return _mapper.Map<ResultOrderItemDto>(item);
+        }
+
+        public async Task CreateAsync(CreateOrderItemDto dto)
+        {
+            var item = _mapper.Map<OrderItem>(dto);
+            await _orderItemRepository.AddAsync(item);
+        }
+
+        public async Task UpdateAsync(UpdateOrderItemDto dto)
+        {
+            var item = _mapper.Map<OrderItem>(dto);
+            await _orderItemRepository.UpdateAsync(item);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var item = await _orderItemRepository.GetByIdAsync(id);
+            if (item != null)
+            {
+                await _orderItemRepository.DeleteAsync(item);
+            }
+        }
+    }
+}
