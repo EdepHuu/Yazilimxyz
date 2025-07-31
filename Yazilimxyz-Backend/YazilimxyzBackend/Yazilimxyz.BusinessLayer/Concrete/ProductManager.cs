db7b61344d10f5bb236cc4adda4d7207aad9a98c
uyanikzahit <146885210+uyanikzahit@users.noreply.github.com>
2025-07-31 14:00:40 +0300

    Services added to the abstract folder in the business layer.

--- Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/ProductManager.cs
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/ProductManager.cs
@@ -1,13 +1,58 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Yazilimxyz.BusinessLayer.Abstract;
+using Yazilimxyz.BusinessLayer.DTOs.Product;
+using Yazilimxyz.EntityLayer.Entities;
 
 namespace Yazilimxyz.BusinessLayer.Concrete
 {
-    public class ProductManager:IProductService
+    public class ProductManager : IProductService
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductManager(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllAsync()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return _mapper.Map<List<ResultProductDto>>(products);
+        }
+
+        public async Task<ResultProductDto> GetByIdAsync(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ResultProductDto>(product);
+        }
+
+        public async Task CreateAsync(CreateProductDto dto)
+        {
+            var product = _mapper.Map<Product>(dto);
+            await _productRepository.AddAsync(product);
+        }
+
+        public async Task UpdateAsync(UpdateProductDto dto)
+        {
+            var product = _mapper.Map<Product>(dto);
+            await _productRepository.UpdateAsync(product);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product != null)
+            {
+                await _productRepository.DeleteAsync(product);
+            }
+        }
     }
+
 }
