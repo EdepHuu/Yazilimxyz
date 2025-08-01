22a6befa3079054c1025628a53f0e13901a34d22
Gülendamcetin <gulendamcetin970@gmail.com>
2025-08-01 23:51:09 +0300

    Appadmin and carditem dtos and mappers were created.

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/DTOs/AppAdmin/CreateAppAdminDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Yazilimxyz.BusinessLayer.DTOs.AppAdmin
+{
+    public class CreateAppAdminDto
+    {
+        public string AppUserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/DTOs/CartItem/ResultCartItemDto.cs
@@ -0,0 +1,15 @@
+﻿using Yazilimxyz.BusinessLayer.DTOs.AppUser;
+using Yazilimxyz.BusinessLayer.DTOs.ProductVariant;
+
+namespace Yazilimxyz.BusinessLayer.DTOs.CartItem
+{
+    public class ResultCartItemDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int ProductVariantId { get; set; }
+        public int Quantity { get; set; }
+        public ResultAppUserDto AppUser { get; set; }
+        public ResultProductVariantDto Variant { get; set; }
+    }
+}
