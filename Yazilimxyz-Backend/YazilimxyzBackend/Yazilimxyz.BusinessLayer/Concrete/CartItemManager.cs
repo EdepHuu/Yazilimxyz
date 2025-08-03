0d4dd2eabf59c2fd07656d00b95118ae430a1166
Mehmet Zahit Uyanik <146885210+uyanikzahit@users.noreply.github.com>
2025-08-03 18:49:29 +0300

    Feature/zahit/managers (#51)
    * Update to services
    
    * Update to managers

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/CartItemManager.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Yazilimxyz.BusinessLayer.Abstract;
+using Yazilimxyz.BusinessLayer.DTOs.CartItem;
+using Yazilimxyz.DataAccessLayer.Abstract;
+using Yazilimxyz.EntityLayer.Entities;
+
+namespace Yazilimxyz.BusinessLayer.Concrete
+{
+    public class CartItemManager : ICartItemService
+    {
+        private readonly ICartItemRepository _cartItemRepository;
+        private readonly IMapper _mapper;
+
+        public CartItemManager(ICartItemRepository cartItemRepository, IMapper mapper)
+        {
+            _cartItemRepository = cartItemRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResultCartItemDto>> GetByUserIdAsync(string userId)
+        {
+            var items = await _cartItemRepository.GetByUserIdAsync(userId);
+            return _mapper.Map<List<ResultCartItemDto>>(items);
+        }
+
+        public async Task<ResultCartItemDto?> GetByUserAndVariantAsync(string userId, int variantId)
+        {
+            var item = await _cartItemRepository.GetByUserAndVariantAsync(userId, variantId);
+            return _mapper.Map<ResultCartItemDto?>(item);
+        }
+
+        public async Task AddAsync(CreateCartItemDto dto)
+        {
+            var item = _mapper.Map<CartItem>(dto);
+            await _cartItemRepository.AddAsync(item);
+        }
+
+        public async Task UpdateAsync(UpdateCartItemDto dto)
+        {
+            var item = _mapper.Map<CartItem>(dto);
+            await _cartItemRepository.UpdateAsync(item);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _cartItemRepository.DeleteAsync(id);
+        }
+
+        public async Task ClearUserCartAsync(string userId)
+        {
+            await _cartItemRepository.ClearUserCartAsync(userId);
+        }
+
+        public async Task<int> GetCartItemCountAsync(string userId)
+        {
+            return await _cartItemRepository.GetCartItemCountAsync(userId);
+        }
+    }
+}
---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/CustomerAddressManager.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Yazilimxyz.BusinessLayer.Abstract;
+using Yazilimxyz.BusinessLayer.DTOs.CustomerAddress;
+using Yazilimxyz.DataAccessLayer.Abstract;
+using Yazilimxyz.EntityLayer.Entities;
+
+namespace Yazilimxyz.BusinessLayer.Concrete
+{
+    public class CustomerAddressManager : ICustomerAddressService
+    {
+        private readonly ICustomerAddressRepository _customerAddressRepository;
+        private readonly IMapper _mapper;
+
+        public CustomerAddressManager(ICustomerAddressRepository customerAddressRepository, IMapper mapper)
+        {
+            _customerAddressRepository = customerAddressRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResultCustomerAddressDto>> GetByCustomerIdAsync(int customerId)
+        {
+            var addresses = await _customerAddressRepository.GetByCustomerIdAsync(customerId);
+            return _mapper.Map<List<ResultCustomerAddressDto>>(addresses);
+        }
+
+        public async Task<ResultCustomerAddressDto?> GetDefaultAddressAsync(int customerId)
+        {
+            var address = await _customerAddressRepository.GetDefaultAddressAsync(customerId);
+            return _mapper.Map<ResultCustomerAddressDto?>(address);
+        }
+
+        public async Task SetDefaultAddressAsync(int customerId, int addressId)
+        {
+            await _customerAddressRepository.SetDefaultAddressAsync(customerId, addressId);
+        }
+
+        public async Task<ResultCustomerAddressDto?> GetByIdAsync(int id)
+        {
+            var address = await _customerAddressRepository.GetByIdAsync(id);
+            return _mapper.Map<ResultCustomerAddressDto?>(address);
+        }
+
+        public async Task CreateAsync(CreateCustomerAddressDto dto)
+        {
+            var address = _mapper.Map<CustomerAddress>(dto);
+            await _customerAddressRepository.AddAsync(address);
+        }
+
+        public async Task UpdateAsync(UpdateCustomerAddressDto dto)
+        {
+            var address = await _customerAddressRepository.GetByIdAsync(dto.Id);
+            if (address != null)
+            {
+                _mapper.Map(dto, address);
+                await _customerAddressRepository.UpdateAsync(address);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _customerAddressRepository.DeleteAsync(id);
+        }
+    }
+}
