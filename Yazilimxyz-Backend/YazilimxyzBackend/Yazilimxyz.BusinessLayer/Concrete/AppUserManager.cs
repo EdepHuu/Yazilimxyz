c24937f93d35c5b56ab821c7db95a47291f0bfe8
Mehmet Zahit Uyanik <146885210+uyanikzahit@users.noreply.github.com>
2025-08-04 11:20:38 +0300

    Update to appusermanager (#53)

--- Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/AppUserManager.cs
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/Concrete/AppUserManager.cs
@@ -1,19 +1,72 @@
-﻿using Yazilimxyz.BusinessLayer.DTOs.AppUser;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Yazilimxyz.BusinessLayer.Abstract;
+using Yazilimxyz.BusinessLayer.DTOs.AppUser;
+using Yazilimxyz.DataAccessLayer.Abstract;
+using Yazilimxyz.EntityLayer.Entities;
 
-namespace Yazilimxyz.BusinessLayer.Abstract
+namespace Yazilimxyz.BusinessLayer.Concrete
 {
-    public interface IAppUserService
+    public class AppUserManager : IAppUserService
     {
-        Task<List<ResultAppUserDto>> GetAllAsync();
-        Task<ResultAppUserDto?> GetByIdAsync(string id);
-        Task<ResultAppUserDto?> GetByEmailAsync(string email);
-        //Task<ResultAppUserWithCustomerDto?> GetWithCustomerAsync(string id);
-        //Task<ResultAppUserWithMerchantDto?> GetWithMerchantAsync(string id);
-        Task<ResultAppUserDto?> CreateAsync(CreateAppUserDto dto);
-        Task<ResultAppUserDto?> UpdateAsync(UpdateAppUserDto dto);
-        Task DeleteAsync(string id);
-        Task<bool> ExistsAsync(string id);
+        private readonly IAppUserRepository _appUserRepository;
+        private readonly IMapper _mapper;
+
+        public AppUserManager(IAppUserRepository appUserRepository, IMapper mapper)
+        {
+            _appUserRepository = appUserRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResultAppUserDto?> GetByIdAsync(string id)
+        {
+            var user = await _appUserRepository.GetByIdAsync(id);
+            return user is not null ? _mapper.Map<ResultAppUserDto>(user) : null;
+        }
+
+        public async Task<ResultAppUserDto?> GetByEmailAsync(string email)
+        {
+            var user = await _appUserRepository.GetByEmailAsync(email);
+            return user is not null ? _mapper.Map<ResultAppUserDto>(user) : null;
+        }
+
+        //public async Task<ResultAppUserWithCustomerDto?> GetWithCustomerAsync(string id)
+        //{
+        //    var user = await _appUserRepository.GetWithCustomerAsync(id);
+        //    return user is not null ? _mapper.Map<ResultAppUserWithCustomerDto>(user) : null;
+        //}
+
+        //public async Task<ResultAppUserWithMerchantDto?> GetWithMerchantAsync(string id)
+        //{
+        //    var user = await _appUserRepository.GetWithMerchantAsync(id);
+        //    return user is not null ? _mapper.Map<ResultAppUserWithMerchantDto>(user) : null;
+        //}
+
+        public async Task<List<ResultAppUserDto>> GetAllAsync()
+        {
+            var users = await _appUserRepository.GetAllAsync();
+            return _mapper.Map<List<ResultAppUserDto>>(users);
+        }
+
+        public async Task<bool> ExistsAsync(string id)
+        {
+            return await _appUserRepository.ExistsAsync(id);
+        }
+
+        public async Task CreateAsync(CreateAppUserDto dto)
+        {
+            var user = _mapper.Map<AppUser>(dto);
+            await _appUserRepository.CreateAsync(user);
+        }
+
+        public async Task UpdateAsync(UpdateAppUserDto dto)
+        {
+            var user = _mapper.Map<AppUser>(dto);
+            await _appUserRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _appUserRepository.DeleteAsync(id);
+        }
     }
 }
