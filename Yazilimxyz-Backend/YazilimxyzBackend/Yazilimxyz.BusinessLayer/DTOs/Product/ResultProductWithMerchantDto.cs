d18b45f94bee9e87309907bbd743968e5fa8d265
Gülendamcetin <101371698+guleecetin@users.noreply.github.com>
2025-08-05 22:28:30 +0300

    Feature/gulendam/dto update (#69)
    * Update DTOs for product and user
    
    * Update DTOs for product and merchant

--- Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/DTOs/Product/ResultProductWithMerchantDto.cs
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/DTOs/Product/ResultProductWithMerchantDto.cs
@@ -1,12 +1,31 @@
 ﻿using Yazilimxyz.BusinessLayer.DTOs.AppUser;
+using Yazilimxyz.EntityLayer.Enums;
+
+
 
 namespace Yazilimxyz.BusinessLayer.DTOs.Product
 {
     public class ResultProductWithMerchantDto
     {
+// Ürüne ait bilgiler
         public int Id { get; set; }
         public string Name { get; set; }
-        public ResultAppUserDto AppUser { get; set; }
+        public string Description { get; set; }
+        public decimal BasePrice { get; set; }
+        public GenderType Gender { get; set; }
         public bool IsActive { get; set; }
+        public string ProductCode { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        // Ürünün ait olduğu kategori bilgisi
+        public string CategoryName { get; set; }
+        
+        // Satıcıya ait bilgiler
+        public int MerchantId { get; set; }
+        public string MerchantName { get; set; }
+
+        // Eğer ürün resimlerini de getirmek isterseniz bu listeyi ekleyin.
+        public List<ResultProductImageDto> Images { get; set; } = new();
     }
 }
