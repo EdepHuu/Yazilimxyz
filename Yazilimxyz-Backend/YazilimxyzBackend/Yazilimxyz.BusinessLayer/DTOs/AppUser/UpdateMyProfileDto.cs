b973e18dd1931626fc4ec794a57c3624fc03d80a
Metehan Dündar <91695805+EdepHuu@users.noreply.github.com>
2025-08-13 17:24:34 +0300

    Enhance user management in UserController
    Updated UserController to support user info retrieval, profile updates, email changes, and password management. Injected UserManager<AppUser> for user operations and added new endpoints for email change requests and confirmations. Adjusted authorization to restrict access to authenticated users. Created/modified DTOs for data transfer with validation rules.

---
+++ Yazilimxyz-Backend/YazilimxyzBackend/Yazilimxyz.BusinessLayer/DTOs/AppUser/UpdateMyProfileDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yazilimxyz.BusinessLayer.DTOs.AppUser
+{
+	public class UpdateMyProfileDto
+	{
+		[StringLength(100)] public string? Name { get; set; }
+		[StringLength(100)] public string? LastName { get; set; }
+		[StringLength(20)] public string? Phone { get; set; }
+	}
+}
