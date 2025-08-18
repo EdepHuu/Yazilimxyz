31a74ae0f2e92260ab8f5bce41db320aa8fe2d5e
cogulbusra <b.cogul@urunver.com.tr>
2025-08-18 23:19:40 +0300

    feat: add product page

--- Yazilimxyz-Frontend/frontend-web/src/app/customer/urunler/page.tsx
+++ Yazilimxyz-Frontend/frontend-web/src/app/customer/urunler/page.tsx
@@ -259,6 +259,8 @@ function UrunlerPage() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0">
               {products.map((product) => (
                 <ProductCard key={product.id} product={product} />
+
+                
               ))}
             </div>
           </div>
