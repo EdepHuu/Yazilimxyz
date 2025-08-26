72e2136fa09ef98df07f0262318b75d208841caf
cogulbusra <b.cogul@urunver.com.tr>
2025-08-26 23:32:01 +0300

    updated

--- Yazilimxyz-Frontend/frontend-web/src/app/customer/urunDetay/[id]/page.tsx
+++ Yazilimxyz-Frontend/frontend-web/src/app/customer/urunDetay/[id]/page.tsx
@@ -229,7 +229,7 @@ export default function ProductDetailPage() {
             alt={product.description}
             width={100}
             height={100}
-            className="rounded object-cover w-36 h-40"
+            className="rounded object-cover w-36 h-40  "
           />
         ))}
       </div>
